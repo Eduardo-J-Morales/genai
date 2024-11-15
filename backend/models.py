a3ddd3c03421e109f3e9422d926ee12711a62b1e
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-15 15:46:13 +0000

    now coded the function inside the model responsable for returning the actual data

--- backend/models.py
+++ backend/models.py
@@ -6,4 +6,8 @@ class Contact(db.Model):
     last_name = db.column(db.string(80), unique=False, nullable=False)
     
     def to_json(self):
-        
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+        }
