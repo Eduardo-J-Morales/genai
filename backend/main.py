63b78bb98c64ac6a828fb5a9a4cbb9cf4c656ad9
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-15 16:17:46 +0000

    Testing the GET method and creating the sqlite database

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,11 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
 
 
 if __name__ == "__main__":
