7fb87d80436247d759772902445b49240eb252a6
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-15 16:36:01 +0000

    Test was successfull

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,10 @@
 from config import db 
 
 class Contact(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.column(db.String(80), unique=False, nullable=False)
-    last_name = db.column(db.string(80), unique=False, nullable=False)
+    last_name = db.column(db.String(80), unique=False, nullable=False)
+    email = db.column(db.String(80), unique=True, nullable=False)
     
     def to_json(self):
         return {
