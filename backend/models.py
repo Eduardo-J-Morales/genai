bb9b4edaa390983dd682c68b6ea19b9aac0ac797
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-15 16:48:37 +0000

    Creating the method POST (create contact)

--- backend/models.py
+++ backend/models.py
@@ -2,13 +2,14 @@ from config import db
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.column(db.String(80), unique=False, nullable=False)
-    last_name = db.column(db.String(80), unique=False, nullable=False)
-    email = db.column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     
     def to_json(self):
         return {
             "id": self.id,
             "first_name": self.first_name,
             "last_name": self.last_name,
+            "email": self.email
         }
